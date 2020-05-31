ba704f930697d0c6436d50a876c58443617090b4
Mayssa <mayssa.hidous@esprit.tn>
2020-05-31 15:58:41 +0100

    Second commit mayssa (jardin)

---
+++ src/main/java/tn/esprit/spring/entity/Jardin.java
@@ -0,0 +1,164 @@
+package tn.esprit.spring.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="T_JARDIN")
+@DiscriminatorValue(value="jd")
+public class Jardin extends User implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="NOM_J")
+	String nomJ;
+	@Column(name="LOGO_J")
+	String logoJ;
+	@Column(name="ADRESSE_J")
+	String adresseJ;
+	@Column(name="NUM_J")
+	String numJ;
+	@Temporal(TemporalType.DATE)
+	private Date dateCrea;
+	@Column(name="DESCRIPTION_J")
+	String descripJ;
+	@Column(name="TARIF_J")
+	float tarifJ;
+	
+	
+	@OneToOne
+	private Directeurs directeurs; 
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy="jardin")
+	private Set<Evenements> Evenements;
+
+	public String getNomJ() {
+		return nomJ;
+	}
+
+	public void setNomJ(String nomJ) {
+		this.nomJ = nomJ;
+	}
+
+	public String getLogoJ() {
+		return logoJ;
+	}
+
+	public void setLogoJ(String logoJ) {
+		this.logoJ = logoJ;
+	}
+
+	public String getAdresseJ() {
+		return adresseJ;
+	}
+
+	public void setAdresseJ(String adresseJ) {
+		this.adresseJ = adresseJ;
+	}
+
+	public String getNumJ() {
+		return numJ;
+	}
+
+	public void setNumJ(String numJ) {
+		this.numJ = numJ;
+	}
+
+	public Date getDateCrea() {
+		return dateCrea;
+	}
+
+	public void setDateCrea(Date dateCrea) {
+		this.dateCrea = dateCrea;
+	}
+
+	public String getDescripJ() {
+		return descripJ;
+	}
+
+	public void setDescripJ(String descripJ) {
+		this.descripJ = descripJ;
+	}
+
+	public float getTarifJ() {
+		return tarifJ;
+	}
+
+	public void setTarifJ(float tarifJ) {
+		this.tarifJ = tarifJ;
+	}
+
+	public Directeurs getDirecteurs() {
+		return directeurs;
+	}
+
+	public void setDirecteurs(Directeurs directeurs) {
+		this.directeurs = directeurs;
+	}
+
+	public Set<Evenements> getEvenements() {
+		return Evenements;
+	}
+
+	public void setEvenements(Set<Evenements> evenements) {
+		Evenements = evenements;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public Jardin() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	public Jardin(String nomJ, String logoJ, String adresseJ, String numJ, Date dateCrea, String descripJ,
+			float tarifJ) {
+		super();
+		this.nomJ = nomJ;
+		this.logoJ = logoJ;
+		this.adresseJ = adresseJ;
+		this.numJ = numJ;
+		this.dateCrea = dateCrea;
+		this.descripJ = descripJ;
+		this.tarifJ = tarifJ;
+	}
+
+	public Jardin(String nomJ, String logoJ, String adresseJ, String numJ, Date dateCrea, String descripJ, float tarifJ,
+			Directeurs directeurs, Set<tn.esprit.spring.entity.Evenements> evenements) {
+		super();
+		this.nomJ = nomJ;
+		this.logoJ = logoJ;
+		this.adresseJ = adresseJ;
+		this.numJ = numJ;
+		this.dateCrea = dateCrea;
+		this.descripJ = descripJ;
+		this.tarifJ = tarifJ;
+		this.directeurs = directeurs;
+		Evenements = evenements;
+	}
+
+	@Override
+	public String toString() {
+		return "Jardin [nomJ=" + nomJ + ", logoJ=" + logoJ + ", adresseJ=" + adresseJ + ", numJ=" + numJ + ", dateCrea="
+				+ dateCrea + ", descripJ=" + descripJ + ", tarifJ=" + tarifJ + ", directeurs=" + directeurs
+				+ ", Evenements=" + Evenements + "]";
+	}
+	
+	
+
+}
--- src/main/java/tn/esprit/spring/services/EvenementsServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EvenementsServiceImpl.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import tn.esprit.spring.entity.Evenements;
 import tn.esprit.spring.repository.EvenementsRepository;
