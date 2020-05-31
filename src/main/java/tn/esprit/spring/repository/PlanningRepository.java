d6edc9fb0fc2a5d49089ed063c4c7e83d4cfddf4
HP <HP@DESKTOP-M6N7GPT>
2020-05-31 01:22:09 +0100

    my commit

---
+++ src/main/java/tn/esprit/spring/repository/PlanningRepository.java
@@ -0,0 +1,23 @@
+package tn.esprit.spring.repository;
+
+
+
+import tn.esprit.spring.entity.Planning;
+
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface PlanningRepository extends JpaRepository<Planning, Integer> {
+	@Query("SELECT DISTINCT p FROM Planning p where p. date_debut=:date")
+	
+	   public List<Planning> GetPlByDate(@Param("date") Date date);
+	
+
+
+}
---
+++ src/main/java/tn/esprit/spring/services/PlanningServiceImpl.java
@@ -0,0 +1,79 @@
+package tn.esprit.spring.services;
+
+import java.util.Date;
+import java.util.List;
+
+
+
+import tn.esprit.spring.entity.Planning;
+
+import tn.esprit.spring.entity.User;
+import tn.esprit.spring.repository.EnfantRepository;
+import tn.esprit.spring.repository.PlanningRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+@Service
+public class PlanningServiceImpl implements PlanningSerivce {
+	@Autowired
+	PlanningRepository planningrepo;
+	
+	@Autowired
+	EnfantRepository erepo;
+	
+	
+	
+	@Override
+	public int AddPlanning(Planning p) {
+		planningrepo.save(p);
+		return 1;
+	}
+
+	
+
+	@Override
+	public void DeletePlanning(int id) {
+		Planning planning =  planningrepo.findById(id).get();
+		planningrepo.delete(planning);
+		
+		
+		}
+
+	@Override
+	public Planning updatePlannig(Planning p) {
+		 planningrepo.save(p);
+		 return p;
+	}
+
+	@Override
+	public List<Planning> GetPlByDate(Date date) {
+		return planningrepo.GetPlByDate(date);
+		
+	}
+
+
+
+
+
+	@Override
+	public String EnvoiPlanning() {	
+		return null;
+		
+		
+	}
+
+
+
+	@Override
+	public List<User> parents(Long id) {
+	  return erepo.parents(id);
+	}
+	@Override
+	public List<Planning> Getallplan() {
+		return planningrepo.findAll();
+		
+	}
+
+
+}
